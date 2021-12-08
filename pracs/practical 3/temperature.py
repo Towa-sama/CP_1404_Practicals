b91818a2eccca36b85c852f40d9e9b8d2100cafa
Towa-sama <assanali.amangeldiyev@my.jcu.edu.au>
2021-12-08 08:50:35 +0600

    Meaningful commit message

---
+++ pracs/practical 3/temperature.py
@@ -0,0 +1,38 @@
+MENU = """C - Convert Celsius to Fahrenheit
+  F - Convert Fahrenheit to Celsius
+  Q - Quit"""
+
+
+def main():
+    print(MENU)
+
+
+def convert_to_fahrenheit(celcius):
+    celcius * 9.0 / 5 + 32
+    return celsius
+
+
+def convert_to_celcius(fahrenheit):
+    5 / 9 * (fahrenheit - 32)
+    return fahrenheit
+
+
+choice = input(">>> ").upper()
+
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = convert_to_fahrenheit(celsius)
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        fahrenheit = float(input("Fahrenheit: "))
+        celsius = convert_to_celcius(fahrenheit)
+        print("Result: {:.2f} C".format(celsius))
+        pass
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
+
+main()

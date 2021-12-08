e97027162d4f0e1f5005481ab502fb3de9437c1f
Towa-sama <assanali.amangeldiyev@my.jcu.edu.au>
2021-12-08 08:43:11 +0600

    Example of commit

---
+++ pracs/samples/examples.py
@@ -0,0 +1,8 @@
+my_subjects = {'CP1401', 'CP1404', 'MA1000'}
+your_subjects = {'CP1404', 'MA1008', 'NM1010'}
+
+
+print(my_subjects - your_subjects)
+print(my_subjects | your_subjects)
+print(my_subjects & your_subjects)
+print(my_subjects ^ your_subjects)
